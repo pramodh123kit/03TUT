4384ba71e90930544b01bd07823b5a806ee7ca2a
Pramodh <pramodh123kit@gmail.com>
2023-10-10 21:38:13 +0530

    Node.js emitter

---
+++ index.js
@@ -0,0 +1,18 @@
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+// Initialize object
+
+const myEmitter = new MyEmitter();
+
+// add Listener for the log event 
+myEmitter.on('log',(msg) => logEvents(msg));
+
+setTimeout(() => {
+  // Emit event
+
+  myEmitter.emit('log', 'log event emitted!');
+},2000);
